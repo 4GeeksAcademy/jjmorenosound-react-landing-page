85d9cafae8ecc60a468fc611dba116969ac0dc93
Jorge Moreno <jjmorenoaudio@gmail.com>
2024-11-06 19:52:25 +0000

    first commit

---
+++ src/js/component/card.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types"; 
+import AngelImage from "../../img/angeluscard.jpg";
+import BuffyImage from "../../img/buffycard.jpg";
+import SpikeImage from "../../img/spikecard.jpg";
+import WillowImage from "../../img/willowbadcard.jpg";
+
+
+const cards = [
+  {
+    imageUrl: AngelImage,
+    title: "Angel",
+    description: "The baddest vampire if soulless",
+    buttonUrl: "https://www.youtube.com/watch?v=vpAol_17mO0&pp=ygULYW5nZWwgaW50cm8%3D",
+    buttonLabel: "Check it"
+  },
+  {
+    imageUrl: BuffyImage,
+    title: "Buffy",
+    description: "The vampire slayer",
+    buttonUrl: "https://www.youtube.com/watch?v=EsFPbVwNJi8&pp=ygUZYnVmZnkgdGhlbWUgc29uZyBzZWFzb24gMw%3D%3D",
+    buttonLabel: "Check it"
+  },
+  {
+    imageUrl: SpikeImage,
+    title: "Spike",
+    description: "The meanest vampire with a soul",
+    buttonUrl: "https://www.youtube.com/watch?v=HAdndhd8OsE&pp=ygUUb3V0IGZvciBhIHdhbGsgc3Bpa2U%3D",
+    buttonLabel: "Check it"
+  },
+  {
+    imageUrl: WillowImage,
+    title: "Willow",
+    description: "The big bad witch",  // Corrected "which" to "witch"
+    buttonUrl: "https://www.youtube.com/watch?v=FWvpx7l-Fa8&pp=ygUMd2lsbG93IGJ1ZmZ5",
+    buttonLabel: "Check it"
+  }
+];
+
+
+export const BootstrapCard = () => {
+  return (
+    <div className="card-deck">
+      {cards.map((obj, index) => (
+        <div key={index} className="card" style={{ width: "18rem" }}>
+          <img src={obj.imageUrl} className="card-img-top" alt={obj.title} />
+          <div className="card-body">
+            <h5 className="card-title">{obj.title}</h5>
+            <p className="card-text">{obj.description}</p>
+            <a href={obj.buttonUrl} className="btn btn-primary">
+              {obj.buttonLabel}
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+
+BootstrapCard.propTypes = {
+  title: PropTypes.string,
+  imageUrl: PropTypes.string,
+  description: PropTypes.string,
+  buttonUrl: PropTypes.string,
+  buttonLabel: PropTypes.string
+};
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import { BootstrapCard } from "./card";
+
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
+		<>
+		{/* <div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
@@ -19,7 +22,9 @@ const Home = () => {
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
 			</p>
-		</div>
+		</div> */}
+		<BootstrapCard></BootstrapCard>
+		</>
 	);
 };
 
